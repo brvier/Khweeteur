f8a613d20275531153a7f68c325973eeaadd78e5
Neal H. Walfield <neal@walfield.org>
2011-08-09 23:54:04 +0200

    daemon: Properly daemonize: use python-daemon.
     - Include version 1.5.5 of python-daemon.
      - Include minor customizations.  Diff in 1.5.5.diff.
     - Include version 0.8 of lockfile (0.9.1 doesn't work with python-daemon)

--- khweeteur/daemon.py
+++ khweeteur/daemon.py
@@ -43,6 +43,9 @@ except:
 import os.path
 import dbus.service
 
+from pydaemon.runner import DaemonRunner
+from lockfile import LockTimeout
+
 try:
     from QtMobility.Location import QGeoPositionInfoSource
 except:
@@ -103,169 +106,6 @@ def settings_db():
         settings_db_generation += 1
 
     return _settings
-
-class Daemon(object):
-
-    """
-    A generic daemon class.
-    Usage: subclass the Daemon class and override the run() method
-    """
-
-    def __init__(
-        self,
-        pidfile,
-        stdin='/dev/null',
-        stdout='/dev/null',
-        stderr='/dev/null',
-        ):
-        self.stdin = stdin
-        self.stdout = stdout
-        self.stderr = stderr
-        self.pidfile = pidfile
-
-    def daemonize(self):
-        """
-        do the UNIX double-fork magic, see Stevens' "Advanced
-        Programming in the UNIX Environment" for details (ISBN 0201563177)
-        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
-        """
-
-        try:
-            pid = os.fork()
-            if pid > 0:
-
-                # exit first parent
-
-                sys.exit(0)
-        except OSError, e:
-            sys.stderr.write('fork #1 failed: %d (%s)\n' % (e.errno,
-                             e.strerror))
-            sys.exit(1)
-
-        # decouple from parent environment
-
-        os.chdir('/')
-        os.setsid()
-        os.umask(0)
-
-        # do second fork
-
-        try:
-            pid = os.fork()
-            if pid > 0:
-
-                # exit from second parent
-
-                sys.exit(0)
-        except OSError, e:
-            sys.stderr.write('fork #2 failed: %d (%s)\n' % (e.errno,
-                             e.strerror))
-            sys.exit(1)
-
-        # redirect standard file descriptors
-
-        sys.stdout.flush()
-        sys.stderr.flush()
-        si = file(self.stdin, 'r')
-        so = file(self.stdout, 'a+')
-        se = file(self.stderr, 'a+', 0)
-        os.dup2(si.fileno(), sys.stdin.fileno())
-        os.dup2(so.fileno(), sys.stdout.fileno())
-        os.dup2(se.fileno(), sys.stderr.fileno())
-
-        # write pidfile
-
-        atexit.register(self.delpid)
-        pid = str(os.getpid())
-        file(self.pidfile, 'w+').write('%s\n' % pid)
-
-    def delpid(self):
-        os.remove(self.pidfile)
-
-    def startfromprefs(self):
-        settings = settings_db()
-        if settings.contains('useDaemon'):
-            if settings.value('useDaemon') == '2':
-                self.start()
-
-    def start(self):
-        """
-        Start the daemon
-        """
-        # Check for a pidfile to see if the daemon already runs
-        try:
-            pf = file(self.pidfile, 'r')
-            pid = int(pf.read().strip())
-            pf.close()
-        except IOError:
-            pid = None
-
-        if pid:
-            try:
-                os.kill(pid, 0)
-                message = 'pidfile %s already exist. Daemon already running?\n'
-                sys.stderr.write(message % self.pidfile)
-                sys.exit(1)
-            except OSError:
-                sys.stderr.write('pidfile %s already exist. But daemon is dead.\n'
-                                  % self.pidfile)
-
-        # Start the daemon
-
-        self.daemonize()
-        self.run()
-
-    def debug(self):
-        self.run()
-        
-    def stop(self):
-        """
-        Stop the daemon
-        """
-
-        # Get the pid from the pidfile
-
-        try:
-            pf = file(self.pidfile, 'r')
-            pid = int(pf.read().strip())
-            pf.close()
-        except IOError:
-            pid = None
-
-        if not pid:
-            message = 'pidfile %s does not exist. Daemon not running?\n'
-            sys.stderr.write(message % self.pidfile)
-            return   # not an error in a restart
-
-        # Try killing the daemon process
-
-        try:
-            while 1:
-                os.kill(pid, SIGTERM)
-                time.sleep(0.1)
-        except OSError, err:
-            err = str(err)
-            if err.find('No such process') > 0:
-                if os.path.exists(self.pidfile):
-                    os.remove(self.pidfile)
-            else:
-                logging.error(str(err))
-                sys.exit(1)
-
-    def restart(self):
-        """
-        Restart the daemon
-        """
-
-        self.stop()
-        self.start()
-
-    def run(self):
-        """
-        You should override this method when you subclass Daemon. It will be called after the process has been
-        daemonized by start() or restart().
-        """
-
 
 class KhweeteurDBusHandler(dbus.service.Object):
 
@@ -359,15 +199,10 @@ class KhweeteurDBusHandler(dbus.service.Object):
                 pass
 
 
-class KhweeteurDaemon(Daemon,QCoreApplication):
+class KhweeteurDaemon(QCoreApplication):
+    def __init__(self):
+        pass
 
-    def __init__(self, pidfile,
-        stdin='/dev/null',
-        stdout='/dev/null',
-        stderr='/dev/null',):
-        Daemon.__init__(self,pidfile,stdin=stdin, \
-                        stdout=stdout, \
-                        stderr=stderr)
     def run(self):
         QCoreApplication.__init__(self,sys.argv)
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
@@ -1042,26 +877,48 @@ class KhweeteurDaemon(Daemon,QCoreApplication):
 
 if __name__ == '__main__':
     install_excepthook(__version__)
-    if os.path.exists('/var/run/khweeteurd'):
-        daemon = KhweeteurDaemon('/var/run/khweeteurd/khweeteurd.pid')
-    else:
-        daemon = KhweeteurDaemon('/tmp/khweeteurd.pid')
-    if len(sys.argv) == 2:
-        if 'startfromprefs' == sys.argv[1]:
-            daemon.startfromprefs()
-        elif 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        elif 'debug' == sys.argv[1]:
-            daemon.debug()
-        else:
-            logging.error('Unknown command')
-            print 'Unknown command'
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print 'usage: %s start|stop|restart|startfromprefs|debug' % sys.argv[0]
+
+    def usage(exit_status=2):
+        print ('usage: %s start|stop|restart|startfromprefs|debug'
+               % sys.argv[0])
         sys.exit(2)
+
+    if len(sys.argv) != 2:
+        usage()
+
+    detach_process = None
+    if sys.argv[1] == 'debug':
+        detach_process = False
+        sys.argv[1] = 'start' 
+
+    if 'startfromprefs' == sys.argv[1]:
+        if settings_db().value('useDaemon') == '2':
+            sys.argv[1] = 'start'
+        else:
+            sys.exit(0)
+
+    if sys.argv[1] in ['start', 'stop', 'restart']:
+        khweeteurdaemon = KhweeteurDaemon()
+
+        if detach_process is None:
+            khweeteurdaemon.stdin_path = "/dev/null"
+            khweeteurdaemon.stdout_path = os.path.expanduser(
+                '~/.khweeteur.log.out')
+            khweeteurdaemon.stderr_path = os.path.expanduser(
+                '~/.khweeteur.log.err')
+
+        khweeteurdaemon.pidfile_path = os.path.expanduser('~/.khweeteur.pid')
+        khweeteurdaemon.pidfile_timeout = 3
+
+        khweeteurdaemon.detach_process = detach_process
+
+        runner = DaemonRunner(khweeteurdaemon)
+        try:
+            runner.do_action()
+        except LockTimeout:
+            print "Failed to acquire lock."
+            sys.exit(1)
+    else:
+        logging.error('Unknown command')
+        print "Unknown command: '%s'" % (sys.argv[1],)
+        usage(2)
